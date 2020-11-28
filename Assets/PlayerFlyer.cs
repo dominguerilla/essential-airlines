4b3c98272862c09fbe6744cd000d4c6e719abcd7
cdom <dominguerilla@gmail.com>
2020-11-28 11:26:24 -0500

    Control plane roll with Q and E

--- Assets/PlayerFlyer.cs
+++ Assets/PlayerFlyer.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerFlyer : MonoBehaviour
 {
     public float thrust = 10f;
+    public float rollSpeed = 1f;
     InputActionAsset inputMap;
     
     // Start is called before the first frame update
@@ -27,6 +28,7 @@ public class PlayerFlyer : MonoBehaviour
     {
         transform.Translate(transform.InverseTransformDirection(transform.forward) * Time.deltaTime * thrust);
         Vector2 moveDirection = inputMap["Move"].ReadValue<Vector2>();
-        transform.Rotate(moveDirection.y, moveDirection.x, 0);
+        float rollDirection = inputMap["Roll"].ReadValue<float>();
+        transform.Rotate(moveDirection.y, moveDirection.x, rollDirection * rollSpeed);
     }
 }
