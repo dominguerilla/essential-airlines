e312e943c88a89aa44a06e6dda662efaddcf3a0e
cdom <dominguerilla@gmail.com>
2020-11-28 16:27:44 -0500

    Can drop and collect a delivery

---
+++ Assets/DropTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTarget : MonoBehaviour
+{
+    public void Collect(DroppedObject drop)
+    {
+        Debug.Log($"Collected { drop.gameObject.name }!");
+        Destroy(drop.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        DroppedObject drop = other.GetComponent<DroppedObject>();
+        if (drop)
+        {
+            Collect(drop);
+        }
+    }
+}
---
+++ Assets/DroppedObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppedObject : MonoBehaviour
+{
+    [SerializeField] float dropSpeed = 9.81f;
+
+    bool _isDropping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Drop();
+    }
+
+    void Drop()
+    {
+        StartCoroutine(StartDrop(dropSpeed));
+    }
+
+    IEnumerator StartDrop(float dropSpeed)
+    {
+        _isDropping = true;
+        while (_isDropping)
+        {
+            transform.position -= new Vector3(0, dropSpeed * Time.deltaTime, 0);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
