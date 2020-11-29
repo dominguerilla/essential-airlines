9d3e714b02f4f7a3198540e39a34cd4e0a0a2503
cdom <dominguerilla@gmail.com>
2020-11-28 23:38:57 -0500

    Randomly spawn customers, disable customers

--- Assets/Scripts/DropTarget.cs
+++ Assets/Scripts/DropTarget.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class DropTarget : MonoBehaviour
 {
+    [SerializeField] Material enabledMaterial;
+    [SerializeField] Material disabledMaterial;
+    [SerializeField] Renderer[] renderers;
+
     Scoreboard scoreboard;
+
+    bool _isEnabled = true;
 
     private void Start()
     {
         scoreboard = GameObject.FindObjectOfType<Scoreboard>();
+        if (gameObject.activeSelf)
+        {
+            EnableTarget();
+        }
+        else
+        {
+            DisableTarget();
+        }
     }
+
     public void Collect(DroppedObject drop)
     {
         Debug.Log($"Collected { drop.gameObject.name }!");
         Destroy(drop.gameObject);
-        Destroy(this.gameObject);
+        DisableTarget();
+    }
+
+    public void EnableTarget()
+    {
+        _isEnabled = true;
+        foreach (Renderer render in renderers)
+        {
+            render.material = enabledMaterial;
+        }
+    }
+
+    public void DisableTarget()
+    {
+        _isEnabled = false;
+        foreach (Renderer render in renderers)
+        {
+            render.material = disabledMaterial;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!_isEnabled) return;
         DroppedObject drop = other.GetComponent<DroppedObject>();
         if (drop)
         {
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public bool spawnCustomersOnStart = false;
+
+    [Header("Spawn Prefabs")]
+    public GameObject customerPrefab;
+    public int minNumberOfCustomers = 4;
+    public int maxNumberOfCustomers = 12;
+    public GameObject restaurantPrefab;
+
+    [Header("Debug Cube")]
+    public Vector3 cubeOffset;
+    public Vector3 cubeSize = new Vector3(1, 1, 1);
+    public Color cubeColor;
+
+    private void Start()
+    {
+        if (spawnCustomersOnStart)
+        {
+            SpawnCustomers(Random.Range(minNumberOfCustomers, maxNumberOfCustomers));
+        }
+    }
+
+    public void SpawnCustomers(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 position = GetRandomOffset();
+            GameObject.Instantiate<GameObject>(customerPrefab, transform.position + position, customerPrefab.transform.rotation);
+        }
+    }
+
+    Vector3 GetRandomOffset()
+    {
+        float x = Random.Range(-cubeSize.x/2, cubeSize.x/2);
+        float z = Random.Range(-cubeSize.z/2, cubeSize.z/2);
+        return new Vector3(x, 0, z);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = cubeColor;
+        Gizmos.DrawCube(transform.position + cubeOffset, cubeSize);
+    }
+}
