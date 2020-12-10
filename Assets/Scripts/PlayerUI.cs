6a530b47935dd6ab6edac00e29f9c46bf46c4c3c
cdom <dominguerilla@gmail.com>
2020-12-10 16:51:52 -0500

    Add start instructions and countdown

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
-    [SerializeField] GameObject playerObject;
+    [SerializeField] PlayerInput player;
     [SerializeField] GameObject inGameUI;
     [SerializeField] GameObject pauseMenuUI;
+    [SerializeField] GameObject startScreenUI;
+    [SerializeField] Text startScreenTimer;
     [SerializeField] Texture2D cursorTexture;
     [SerializeField] Vector2 cursorOffset;
 
+    float startingPauseDuration = 5f;
     InputActionAsset inputMap;
     bool _isPaused = false;
 
-    CursorLockMode previousLockState;
+    CursorLockMode _previousLockState;
     bool _wasCursorVisible;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (playerObject == null) 
+        if (player == null) 
         {
             Debug.LogError($"Player not set on {gameObject.name}!");
             Destroy(this);
             return;
         }
-
+        
         InitControls();
+        InitLevel();
     }
 
     public void TogglePause(InputAction.CallbackContext context)
@@ -47,7 +52,7 @@ public class PlayerUI : MonoBehaviour
         _isPaused = true;
         Time.timeScale = 0;
 
-        previousLockState = Cursor.lockState;
+        _previousLockState = Cursor.lockState;
         _wasCursorVisible = Cursor.visible;
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
@@ -61,7 +66,7 @@ public class PlayerUI : MonoBehaviour
         inGameUI.SetActive(true);
         pauseMenuUI.SetActive(false);
 
-        Cursor.lockState = previousLockState;
+        Cursor.lockState = _previousLockState;
         Cursor.visible = _wasCursorVisible;
 
         Time.timeScale = 1;
@@ -70,11 +75,41 @@ public class PlayerUI : MonoBehaviour
 
     void InitControls()
     {
+        Cursor.SetCursor(cursorTexture, cursorOffset, CursorMode.Auto);
+        inputMap = player.GetComponent<PlayerInput>().actions;
+        inputMap["Pause"].performed += TogglePause;
+    }
+
+    void InitLevel()
+    {
+        player.DeactivateInput();
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        Cursor.SetCursor(cursorTexture, cursorOffset, CursorMode.Auto);
 
-        inputMap = playerObject.GetComponent<PlayerInput>().actions;
-        inputMap["Pause"].performed += TogglePause;
+        startScreenUI.SetActive(true);
+        StartCoroutine(StartLevel(startingPauseDuration));
+    }
+
+    IEnumerator StartLevel(float pauseDuration)
+    {
+        float timePassed = 0f;
+        while (timePassed < pauseDuration)
+        {
+            timePassed += Time.unscaledDeltaTime;
+            var timeLeft = (decimal)(pauseDuration - timePassed);
+
+            timeLeft = RoundNum(timeLeft);
+
+            startScreenTimer.text =  timeLeft + "";
+            yield return null;
+        }
+        player.ActivateInput();
+        startScreenUI.SetActive(false);
+    }
+
+    decimal RoundNum(decimal num)
+    {
+        return decimal.Truncate(num * 100) / 100;
     }
 }
